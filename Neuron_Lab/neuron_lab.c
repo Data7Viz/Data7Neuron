21f4880aa885d8e8ad37715a4d52fb427b7b6fb2
Data7Viz <data7vizz@gmail.com>
2026-05-12 03:45:41 +0800

    Выбираем эпохи на лету (scanf)

--- Neuron_Lab/neuron_lab.c
+++ Neuron_Lab/neuron_lab.c
@@ -3,14 +3,16 @@ int main ()
 {
 	float x = 0.0f, w = 0.0f, b = 0.0f, s = 0.0f, t = 0.0f;
 	float out = 0.0f, er = 0.0f;
+	int epo = 0;
 
 	printf (" Вход  : "); scanf ("%f", &x);
 	printf (" Вес   : "); scanf ("%f", &w);
 	printf (" Смещ  : "); scanf ("%f", &b);
 	printf (" Цель  : "); scanf ("%f", &t);
 	printf (" Ск_об : "); scanf ("%f", &s);
+	printf (" Эпохи : "); scanf ("%d", &epo);
 
-	for ( int i = 0; i < 10; i ++)
+	for ( int i = 0; i < epo; i ++)
 	{
 		out = (x * w) + b;
 		er = t - out;

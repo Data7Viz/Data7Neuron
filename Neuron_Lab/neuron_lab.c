5431bfd27eadb9cccf977acff5a013959f9cdab2
Data7Viz <data7vizz@gmail.com>
2026-05-12 03:21:58 +0800

    Пользовательский ввод w,x,b,t,s

--- Neuron_Lab/neuron_lab.c
+++ Neuron_Lab/neuron_lab.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 int main ()
 {
-	float x = 10.0; // вход 
-	float w = 0.05; // вес
-	float t = 1.0;  // цель
-	float s = 0.01; // скорость обучения
-	float b = 0.04; // смещение
-	float out = 0.0; // предсказание
-	float er = 0.0; // ошибка
-	
-	for (int i = 0; i <= 10; i ++)
+	float x = 0.0f, w = 0.0f, b = 0.0f, s = 0.0f, t = 0.0f;
+	float out = 0.0f, er = 0.0f;
+
+	printf (" Вход  : "); scanf ("%f", &x);
+	printf (" Вес   : "); scanf ("%f", &w);
+	printf (" Смещ  : "); scanf ("%f", &b);
+	printf (" Цель  : "); scanf ("%f", &t);
+	printf (" Ск_об : "); scanf ("%f", &s);
+
+	for ( int i = 0; i < 10; i ++)
 	{
-		// предсказание
 		out = (x * w) + b;
-		// расстояние до цели 
 		er = t - out;
-		// спуск
-		w += er * s * x;
-		//смещение
+		w += s * er * x;
 		b += s * er;
 	}
-	printf(" w %.3f b %.3f e %.3f\n", w, b, er);
+	printf (" w %.3f b %.3f er %.3f\n", w, b, er);
 	return 0;
 } 
+
 
 
 

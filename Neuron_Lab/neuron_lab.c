920fbe39c00525682c46735432e8288fa24e68a5
Data7Viz <data7vizz@gmail.com>
2026-05-12 04:02:14 +0800

    Добавил цикл (While)

--- Neuron_Lab/neuron_lab.c
+++ Neuron_Lab/neuron_lab.c
@@ -4,7 +4,9 @@ int main ()
 	float x = 0.0f, w = 0.0f, b = 0.0f, s = 0.0f, t = 0.0f;
 	float out = 0.0f, er = 0.0f;
 	int epo = 0;
-
+	
+	while (1)
+	{
 	printf (" Вход  : "); scanf ("%f", &x);
 	printf (" Вес   : "); scanf ("%f", &w);
 	printf (" Смещ  : "); scanf ("%f", &b);
@@ -20,6 +22,7 @@ int main ()
 		b += s * er;
 	}
 	printf (" w %.3f b %.3f er %.3f\n", w, b, er);
+	} 
 	return 0;
 } 
 
